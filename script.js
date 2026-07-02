9a2ece683e4042d420657d1c20fa5353e4441ba5
leprestigebenin <leprestigevinsetspiritueux@gmail.com>
2026-07-02 16:52:21 +0200

    Remove toggleMenu function
    Removed toggleMenu function for hamburger menu.

--- script.js
+++ script.js
@@ -626,11 +626,6 @@ document.addEventListener("DOMContentLoaded",function(){
 // MENU HAMBURGER
 // =====================================
 
-function toggleMenu() {
-    const menu = document.getElementById("menu");
-    menu.classList.toggle("active");
-}
-
 document.querySelectorAll("#menu a").forEach(link => {
     link.addEventListener("click", () => {
         document.getElementById("menu").classList.remove("active");
